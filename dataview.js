f80e14694d7f158d1c19c02e01add0f7e8f452ca
Graham Roth <graham.s.roth@gmail.com>
2014-12-14 19:54:39 -0800

    Basic functionality down

--- dataview.js
+++ dataview.js
@@ -4,29 +4,89 @@
 
   var activeTab = null;
 
+  var dataPopulated = {};
 
+  function showTabData( elem, old ) {
+
+    if( old !== null ) {
+      var shown = document.getElementById( "color-set-" + old.innerText );
+      shown.style.display = "none";
+    }
+
+    if( elem === null ) return;
+
+    var num = elem.innerText;
+    
+    if( dataPopulated[num] ) {
+      var hidden = document.getElementById( "color-set-" + num );
+      hidden.style.display = "block";
+      return;
+    }
+
+    var box = document.createElement( "div" );
+    box.className = "orderings-container";
+    box.id = "color-set-" + num;
+
+    var dataToShow = data.responses[ "set-" + num ];
+
+    for( var key in dataToShow ) {
+      var ordering = document.createElement( "div" );
+      ordering.className = "color-ordering";
+
+      var set = dataToShow[ key ].colors;
+
+      for( var i = 0; i < set.length; i++ ) {
+
+        var block = document.createElement( "div" );
+        block.className = "block";
+        block.style.backgroundColor = set[i];
+
+        ordering.appendChild( block );
+
+      }
+
+      box.appendChild( ordering );
+
+    }
+
+    var container = document.getElementById( "colors" );
+    container.appendChild( box );
+
+    dataPopulated[num] = true;
+
+  }
 
   function selectTab( e ) {
+
+    var old = activeTab;
 
     if( e.target === activeTab ) {
       activeTab = null;
       e.target.classList.remove( "selected" );
+      showTabData( null, old );
     } else {
       if( activeTab !== null ) activeTab.classList.remove( "selected" );
       activeTab = e.target;
       activeTab.classList.add( "selected" );
+      showTabData( e.target, old );
     }
   }
 
-  for( var i = 0; i < data[ "color-sets" ].size; i++ ) {
+  function buildHeader() {
+    for( var i = 0; i < data[ "color-sets" ].size; i++ ) {
 
-    var navElem = document.createElement( "button" );
-    navElem.className = "nav-element";
-    navElem.innerHTML = i + 1;
+      var navElem = document.createElement( "button" );
+      navElem.className = "nav-element";
+      navElem.innerHTML = i + 1;
 
-    navElem.addEventListener( "click", selectTab );
+      navElem.addEventListener( "click", selectTab );
 
-    nav.appendChild( navElem );
+      nav.appendChild( navElem );
 
+    }
   }
+
+  buildHeader();
+
+
 })();
